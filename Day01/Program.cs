bd4ae5db521bff70edca02e25e7c97ce6dea4c6b
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-17 10:37:18 -0600

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            var frequency = data.Where(s => s != "").Sum(int.Parse);
+            Console.WriteLine("Final frequency is " + frequency);
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
