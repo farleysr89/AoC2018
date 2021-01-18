ca8daeb3d01711bc2f31e47b5812290a06d81331
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-17 18:39:51 -0600

    Day 03 Part 1 Complete

---
+++ Day03/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day03
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        private static void SolvePart1()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            var map = new List<List<int>>();
+            for (var y = 0; y < 1; y++)
+            {
+                map.Add(new List<int>());
+                for (var x = 0; x < 1; x++)
+                {
+                    map[y].Add(0);
+                }
+            }
+
+            foreach (var s in data.Where(s => s != ""))
+            {
+                var parts = s.Split(" @ ");
+                parts = parts[1].Split(": ");
+                var offsets = parts[0].Split((","));
+                var xOffset = int.Parse(offsets[0]);
+                var yOffset = int.Parse(offsets[1]);
+
+                var dimensions = parts[1].Split("x");
+                var width = int.Parse(dimensions[0]);
+                var height = int.Parse(dimensions[1]);
+                var xMax = xOffset + width;
+                var yMax = yOffset + height;
+                if (xMax > map[0].Count)
+                {
+                    foreach (var l in map)
+                    {
+                        while (l.Count < xMax) l.Add(0);
+                    }
+                }
+
+                if (yMax > map.Count)
+                {
+                    for (var y = map.Count; y < yMax; y++)
+                    {
+                        map.Add(new List<int>());
+                        while (map[y].Count < xMax) map[y].Add(0);
+                    }
+                }
+                for (var x = xOffset; x < xOffset + width; x++)
+                {
+                    for (var y = yOffset; y < yOffset + height; y++)
+                    {
+                        map[y][x]++;
+                    }
+                }
+            }
+
+            var count = map.SelectMany(c => c).Count(cc => cc > 1);
+            Console.WriteLine(count);
+        }
+
+        private static void SolvePart2()
+        {
+            var input = File.ReadAllText("Input.txt");
+            var data = input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
