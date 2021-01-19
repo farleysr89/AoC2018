e924d717e7b800dba6c86a9b763b9f52283fc6aa
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-19 16:52:21 -0600

    Day 04 Part 2 Complete

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -56,8 +56,41 @@ namespace Day04
         private static void SolvePart2()
         {
             var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var data = input.Split('\n').Where(s => s != "").OrderBy(s => s).ToList();
+            var guards = new List<Guard>();
+            Guard g = null;
+            var start = 0;
+            foreach (var parts in data.Select(s => s.Split(" ")))
+            {
+                switch (parts[2])
+                {
+                    case "Guard":
+                        var id = int.Parse(parts[3][1..]);
+                        if (guards.Any(a => a.Id == id)) g = guards.First(a => a.Id == id);
+                        else
+                        {
+                            g = new Guard { Id = int.Parse(parts[3][1..]) };
+                            guards.Add(g);
+                        }
+
+                        break;
+                    case "falls":
+                        start = int.Parse(parts[1].Split(":")[1][..^1]);
+                        break;
+                    case "wakes":
+                        var finish = int.Parse(parts[1].Split(":")[1][..^1]);
+                        for (var i = start; i < finish; i++) g.SleepTimes[i]++;
+                        break;
+                    default:
+                        Console.WriteLine("Something broke!");
+                        break;
+                }
+            }
+
+            g = guards.OrderByDescending(g => g.SleepTimes.Max(x => x)).First();
+            var m = g.SleepTimes.Max();
+            var index = g.SleepTimes.ToList().IndexOf(m);
+            Console.WriteLine("Solution = " + (g.Id * index));
         }
     }
 
