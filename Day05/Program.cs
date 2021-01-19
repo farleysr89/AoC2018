3fe89df407af5af937ea86e817a90a2d098f9d38
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-19 17:26:47 -0600

    Day 05 Part 2 Complete

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace Day05
 {
@@ -8,11 +7,11 @@ namespace Day05
     {
         private static void Main()
         {
-            SolvePart1();
-            SolvePart2();
+            var s = SolvePart1();
+            SolvePart2(s);
         }
 
-        private static void SolvePart1()
+        private static string SolvePart1()
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n')[0];
@@ -45,13 +44,53 @@ namespace Day05
                 data = new string(tmp);
             }
             Console.WriteLine("Solution is " + data.Length);
+            return data;
         }
 
-        private static void SolvePart2()
+        private static void SolvePart2(string data)
         {
-            var input = File.ReadAllText("Input.txt");
-            var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var safeData = data;
+
+            var minSize = int.MaxValue;
+
+            for (var r = 'a'; r <= 'z'; r++)
+            {
+                data = new string(safeData);
+                data = data.Replace(r.ToString(), "");
+                data = data.Replace(char.ToUpper(r).ToString(), "");
+                var change = true;
+                while (change)
+                {
+                    change = false;
+                    var tmp = "";
+                    for (var i = 0; i < data.Length; i++)
+                    {
+                        var c = data[i];
+                        if (i == data.Length - 1)
+                        {
+                            tmp += c;
+                            break;
+                        }
+
+                        var cc = data[i + 1];
+                        if (c != cc && (char.ToLower(c)) == char.ToLower(cc))
+                        {
+                            i++;
+                            change = true;
+                        }
+                        else
+                        {
+                            tmp += c;
+                        }
+                    }
+
+                    data = new string(tmp);
+                }
+
+                minSize = Math.Min(minSize, data.Length);
+            }
+
+            Console.WriteLine("Solution is " + minSize);
         }
     }
 }
