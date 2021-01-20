a2513f29bc232891f3e6efb2019f53aff5622d65
Farley <33631912+farleysr89@users.noreply.github.com>
2021-01-19 19:46:29 -0600

    Day 07 Part 2 Complete

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -57,7 +57,66 @@ namespace Day07
         {
             var input = File.ReadAllText("Input.txt");
             var data = input.Split('\n').ToList();
-            Console.WriteLine("");
+            var steps = new List<Step>();
+            foreach (var s in data.Where(s => s != ""))
+            {
+                var parts = s.Split(" ");
+                var pre = parts[1][0];
+                var end = parts[7][0];
+                if (steps.All(ss => ss.Id != end))
+                {
+                    var step = new Step { Id = end, Prerequisites = new List<char>(pre), Time = end - 4 };
+                    step.Prerequisites.Add(pre);
+                    steps.Add(step);
+                }
+                else
+                {
+                    steps.First(ss => ss.Id == end).Prerequisites.Add(pre);
+                }
+                if (steps.All(ss => ss.Id != pre))
+                {
+                    steps.Add(new Step { Id = pre, Prerequisites = new List<char>(), Time = pre - 4 });
+                }
+            }
+
+            var workers = new List<Worker>
+            {
+                new Worker(),
+                new Worker(),
+                new Worker(),
+                new Worker(),
+                new Worker()
+            };
+            var time = 0;
+            while (steps.Any())
+            {
+                foreach (var worker in workers.Where(w => w.InProgress != null))
+                {
+                    worker.TimeLeft--;
+                }
+                foreach (var worker in workers.Where(w => w.InProgress != null && w.TimeLeft == 0))
+                {
+                    foreach (var step in steps.Where(s => s.Prerequisites.Contains(worker.InProgress.Id)))
+                    {
+                        step.Prerequisites.Remove(worker.InProgress.Id);
+                    }
+
+                    worker.InProgress = null;
+                }
+                while (workers.Any(w => w.InProgress == null) && steps.Any(s => s.Prerequisites.Count == 0))
+                {
+                    var work = workers.First(w => w.InProgress == null);
+                    var next = steps.Where(s => s.Prerequisites.Count == 0).OrderBy(s => s.Id).First();
+                    steps.Remove(next);
+                    work.InProgress = next;
+                    work.TimeLeft = next.Time;
+                }
+
+                time++;
+            }
+
+            time += workers.Max(w => w.TimeLeft);
+            Console.WriteLine("Time = " + (time - 1));
         }
     }
 
@@ -65,5 +124,12 @@ namespace Day07
     {
         internal char Id;
         internal List<char> Prerequisites;
+        internal int Time;
+    }
+
+    internal class Worker
+    {
+        internal Step InProgress;
+        internal int TimeLeft;
     }
 }
